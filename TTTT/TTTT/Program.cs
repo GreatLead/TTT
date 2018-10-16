8b532ef5300711157039396df8a3bf831a842ba5
天翔 马 <956682030@qq.com>
2018-10-16 15:00:39 +0800

    a=1

---
+++ TTTT/TTTT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TTTT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 1;
+            Console.Write(a);
+           
+        }
+    }
+}
